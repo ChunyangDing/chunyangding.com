0ad8d71f38da18b3a45b30a9087a515027cd739b
Chunyang Ding <chunyang.ding@gmail.com>
2020-03-29 17:48:05 -0400

    Filled out some of the quotes

--- quotes.php
+++ quotes.php
@@ -11,8 +11,46 @@
 		<div class='container'>
 			<div class='blurb'>
 				<h1>Favorite Quotes</h1>
-				<p> I have been collecting quotes from myriad pop-culture sources for a while now; on Facebook, on Goodreads, and anywhere else that allowed me to. Here's a selection from those! </p>
+				<p> I have been collecting quotes from myriad pop-culture sources for a while now; on Facebook, on Goodreads, and anywhere else that allowed me to. Here's a selection from those! (organized in roughly reverse chronological order, but honestly even I do not know when I gathered some of these lines.)</p>
 				<p> "You're in the story with the rest of us now, and you must go with it, whether you will or no." - The Last Unicorn </p>
+				<p>  "So be it: we live in the present. Whatever community we form that night is ephemeral." ~ Adora Svitak </p>
+				<p>  "The really important kind of freedom involves attention and awareness and discipline, and being able truly to care  about other people and to sacrifice for them over and over in myriad petty, unsexy ways every day." ~ David Foster Wallace, This is Water (commencement at Kenyon, 2005)< /p>
+				<p> "Those of us who are going to live, have to start living by our own lights." - Neal Stephenson, Seveneves </p>
+				<p> "The spring has now come to an end, and it will never return. By winter we will know everything." - Ludvik Vaculik, Two Thousand Words </p>
+				<p> "Let us give up this impossible demand that someone above us must always provide us with the only possible interpretation of things, one simple conclusion. Every single one of us will have to be responsible for arriving at his own conclusions... In the future, we shall have to display personal initiative and determination of our own." - Ludvik Vaculik, Two Thousand Words </p>
+				<p> "But if a man would be alone, let him look at the stars." ~Emerson, Nature </p>
+				<p> "And when they build their statues, they will build none for me." ~ Asimov, "In a Good Cause" </p>
+				<p> "Ah! This morning! You have lived since then." ~ The Portrait of Dorian Gray </p>
+				<p> "We should never abandon the dream of exploring space, but we should also never abandon our fears of it." ~Sophie Ding</p>
+				<p> "The more we realize our minuteness and our impotence in the face of cosmic forces, the more astonishing becomes what human beings have achieved." ~Bertrand Russell </p>
+				<p> "And let your best be for your friend./If he must know the ebb of our tide, let him know its flood also./ For what is your friend that you should seek him with hours to kill?/ Seek him always with hours to live./ For it is his to fill your need, but not your emptiness." ~The Prophet, On Friendship, Kahlil Gibran</p>
+				<p> "But as important as your obligations as a doctor, a lawyer, a business leader will be, you are a human being first. And those human connections - with spouses, with children, with friends - are the most important investments you will ever make."~Barbara Bush, Commencement at Wellesley, 1990</p>
+				<p> "The vast loneliness up here at the moon is awe inspiring, and it makes you realize what you have back on Earth. The Earth from here is a grand oasis in the vastness of space."~Jim Lovell, December 25, 1968</p>
+				<p> "... It is not possible to be a scientist unless you believe that it is good to learn. It is not good to be a scientist, and it is not possible, unless you think that it is of the highest value to share your knowledge, to share it with anyone who is interested. It is not possible to be a scientist unless you believe that the knowledge of the world, and the power which this gives, is a thing which is of intrinsic value to humanity, and that you are using it to help in the spread of knowledge, and are willing to take the consequences." ~J. Robert Oppenheimer, Los Alamos, 2 November 1945 </p>
+				<p> "Within our limitations, there is no limit to how beautiful we can become, how much of our ideal self we can create"~NUMB3RS, Cecil, Welcome to Night Vale </p>
+				<p> "The things that unite us are far stronger than the things that keep us apart."~ David Robertson, Conductor of St. Louis Orchestra and NYO 2014 </p>
+				<p> "For whatever we lose (like a you or a me) // it’s always ourselves we find in the sea."~ E.E. Cummings</p>
+				<p> "If you want to build a ship, don't drum up the men to geather wood, divide the work, and give orders. Instead, teach them to yearn for the vast and endless sea." ~Antoine de Saint-Exupery</p>
+				<p> "A man may die, nations may rise and fall, but an idea lives on."~John F. Kennedy</p>
+				<p> "Of all sad words of mouth or pen, the saddest are these: it might have been."~John Greenleaf Whittier </p>
+				<p> "Too many people undervalue what they are, and overvalue what they are not." ~Kathleen Zhu </p>
+				<p> “They were careless people, Tom and Daisy- they smashed up things and creatures and then retreated back into their money or their vast carelessness or whatever it was that kept them together, and let other people clean up the mess they had made.”~F. Scott Fitzgerald, The Great Gatsby </p>
+				<p> "Well, I can't eat muffins in an agitated manner. The butter would probably get on my cuffs. One should always eat muffins quite calmly. It is the only way to eat them." ~Algernon, The Importance Of Being Earnest </p>
+				<p> “Stay hungry, stay young, stay foolish, stay curious, and above all, stay humble because just when you think you got all the answers, is the moment when some bitter twist of fate in the universe will remind you that you very much don't.”~Tom Hiddleston </p>
+				<p> “Everything I’ve ever let go of has claw marks on it.” ~ David Foster Wallace</p>
+				<p> "Listen to me, Truman. There's no more truth out there than there is in the world I created for you. The same lies, the same deceit. But in my world, you have nothing to fear. ... You're afraid, that's why you can't leave." - The Truman Show</p>
+				<p> “Gatsby believed in the green light, the orgiastic future that year by year recedes before us. It eluded us then, but that's no matter--tomorrow we will run faster, stretch out our arms farther.... And one fine morning-- So we beat on, boats against the current, borne back ceaselessly into the past.” - Great Gatsby </p>
+				<p> "The light that burns twice as bright burns for half as long - and you have burned so very, very brightly" - Blade Runner</p>
+				<p> "And in that moment, I swear we were infinite" - Perks of Being a Wallflower </p>
+				<p> “The atoms of our bodies are traceable to stars that manufactured them in their cores and exploded these enriched ingredients across our galaxy, billions of years ago. For this reason, we are biologically connected to every other living thing in the world. We are chemically connected to all molecules on Earth. And we are atomically connected to all atoms in the universe. We are not figuratively, but literally stardust.”― Neil deGrasse Tyson</p>
+				<p> “The nitrogen in our DNA, the calcium in our teeth, the iron in our blood, the carbon in our apple pies were made in the interiors of collapsing stars. We are made of starstuff.” ― Carl Sagan, Cosmos</p>
+				<p> "Education is what remains after one has forgotten what one has learned in school." -Albert Einstein</p>
+				<p> "I see," he said. "They were part of the tribe. ... The tribe is whatever we believe it is. If we say the tribe is all the Little Ones in the forest, and all the trees, then that is what the tribe is. Even though some of the oldest trees here came from warriors of two different tribes, fallen in battle. We become one tribe because we say we're one tribe." - Speaker for the Dead</p>
+				<p> "Being sad is like being happy for deep people." - Blink, Doctor Who Sea. 3 Ep. 10</p>
+				<p> " 'I'm sorry you can't stay longer,' said Alec sadly. 'There's so much more to see in the Forest of Sight. But I suppose there's a lot to see everywhere, if only you keep your eyes open.' " - The Phantom Tolbooth</p>
+				<p> "A reader lives a thousand lives before he dies, said Jojen. The man who never reads lives only one." - George R. R. Martin</p>
+				<p> Science does not purvey absolute truth, science is a mechanism. It’s a way of trying to improve your knowledge of nature, it’s a system for testing your thoughts against the universe and seeing whether they match. - Isaac Asimov</p>
+				<p> "Life is so beautiful that death has fallen in love with it, a jealous possessive love that grabs at what it can. But life leaps over oblivion lightly, losing only a thing or two of no importance, and gloom is but the passing shadow of a cloud." - Life of Pi</p>
 			</div>
 		</div>
 	<?php include $_SERVER['DOCUMENT_ROOT'].'/assets/footbar.php'; ?>
