96284f02c89a5b473d501c9a1d6757cb03c82015
Chunyang Ding <chunyang.ding@gmail.com>
2020-03-29 23:00:10 -0400

    Update music.php

--- music.php
+++ music.php
@@ -4,6 +4,10 @@
 		<title> Chunyang Ding</title>
 		<link rel="stylesheet" type="text/css" href="/css/main.css">
 		<link rel="stylesheet" type="text/css" href="/css/dropdown.css">
+		<style> 
+		video{
+			transform:rotate(180deg);
+		}
 	</head>
 	
 	<body>
@@ -13,7 +17,7 @@
 				<h1>Guitar Playing</h1>
 				<p> I started learning how to play the guitar in January 2020. My goal is to walk into a crowded room, whip out my acoustic, and say "Anyways, here's Wonderwall." </p>
 				
-				<video width="320" height="240" controls>
+				<video width="600" height=auto controls>
 					<source src="/assets/vids/20200329_LaVieEnRose.mp4" type="video/mp4">
 					Your browser does not support the video tag.
 				</video> 
