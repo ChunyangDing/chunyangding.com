35b1436d2f2b3bc96f4135ebba14dd0ba5b2b079
Chunyang Ding <chunyang.ding@gmail.com>
2020-03-31 23:37:46 -0400

    Oof let's see if this works at all

--- photos.php
+++ photos.php
@@ -5,6 +5,55 @@
 		<title><?php echo $page_name; ?></title>
 		<link rel="stylesheet" type="text/css" href="/css/main.css">
 		<link rel="stylesheet" type="text/css" href="/css/dropdown.css">
+		
+		<style>
+			div.gallery {
+				border: 1px solid #ccc;
+			}
+
+			div.gallery:hover {
+				border: 1px solid #777;
+			}
+
+			div.gallery img {
+				width: 100%;
+				height: auto;
+			}
+
+			div.desc {
+				padding: 15px;
+				text-align: center;
+			}
+
+			* {
+				box-sizing: border-box;
+			}
+
+			.responsive {
+				padding: 0 6px;
+				float: left;
+				width: 24.99999%;
+			}
+
+			@media only screen and (max-width: 700px) {
+				.responsive {
+					width: 49.99999%;
+					margin: 6px 0;
+				}
+			}
+
+			@media only screen and (max-width: 500px) {
+				.responsive {
+					width: 100%;
+				}
+			}
+
+			.clearfix:after {
+				content: "";
+				display: table;
+				clear: both;
+			}
+			</style>
 	</head>
 	
 	<body>
@@ -14,7 +63,79 @@
 				<h1>Photography</h1>
 				<p> Here are some photos that I like </p>
 				
+				
+				<div class="responsive">
+					<div class="gallery">
+						<a target="_blank" href="IMG_20191207_1622475.jpg">
+							<img src="IMG_20191207_1622475.jpg" width="600">
+						</a>
+					</div>
+				</div>
 
+
+				<div class="responsive">
+					<div class="gallery">
+						<a target="_blank" href="IMG_20191219_1327332.jpg">
+							<img src="IMG_20191219_1327332.jpg" width="600">
+						</a>
+					</div>
+				</div>
+
+				<div class="responsive">
+					<div class="gallery">
+						<a target="_blank" href="IMG_20191225_0000000.jpg">
+							<img src="IMG_20191225_0000000.jpg" width="600">
+						</a>
+					</div>
+				</div>
+
+				<div class="responsive">
+					<div class="gallery">
+						<a target="_blank" href="IMG_20191231_1102414.jpg">
+							<img src="IMG_20191231_1102414.jpg" width="600">
+						</a>
+					</div>
+				</div>
+				
+				<div class="responsive">
+					<div class="gallery">
+						<a target="_blank" href="IMG_20191231_1121568.jpg">
+							<img src="IMG_20191231_1121568.jpg" width="600">
+						</a>
+					</div>
+				</div>
+				<div class="responsive">
+					<div class="gallery">
+						<a target="_blank" href="IMG_20200126_1826103-EFFECTS.jpg">
+							<img src="IMG_20200126_1826103-EFFECTS.jpg" width="600">
+						</a>
+					</div>
+				</div>
+				<div class="responsive">
+					<div class="gallery">
+						<a target="_blank" href="IMG_20200223_1701308.jpg">
+							<img src="IMG_20200223_1701308.jpg" width="600">
+						</a>
+					</div>
+				</div>
+				<div class="responsive">
+					<div class="gallery">
+						<a target="_blank" href="IMG_20200319_EFFECTS.jpg">
+							<img src="IMG_20200319_EFFECTS.jpg" width="600">
+						</a>
+					</div>
+				</div>
+				
+				<!--
+				<div class="responsive">
+					<div class="gallery">
+						<a target="_blank" href="img_mountains.jpg">
+							<img src="img_mountains.jpg" width="600">
+						</a>
+					</div>
+				</div>
+				-->
+				
 			</div>
 		</div>
 	<?php include $_SERVER['DOCUMENT_ROOT'].'/assets/webcounter.php';
