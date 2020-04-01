f5f0feecf49050c94c76c2c5cc37c293d1c745e0
Chunyang Ding <chunyang.ding@gmail.com>
2020-03-31 22:15:48 -0400

    button and p are buddies now hopefully

--- assets/footbar.php
+++ assets/footbar.php
@@ -2,7 +2,6 @@
 echo '<script src="/assets/copyVal.js"></script> 
 
 	<footer class="footer">
-		<p> made by Chunyang Ding  |  ©2020  | Page Hits: ', $access_number, '</p>
-		<button class = "emailCopy" onclick="copyVal(\'me@chunyangding.com\')">me@chunyangding.com</button>  
+		<p style="display:inline-block;”> made by Chunyang Ding  |  ©2020  | Page Hits: ', $access_number, ' |</p> <button class = "emailCopy" onclick="copyVal(\'me@chunyangding.com\')">me@chunyangding.com</button>  
 	</footer>';
 ?>
