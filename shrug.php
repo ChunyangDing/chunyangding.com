fc5499b6bbee9de6068629c38390d4750ccfd27e
Chunyang Ding <chunyang.ding@gmail.com>
2020-03-31 21:55:26 -0400

    Looks like the script code needs to be in the body, not head.

--- shrug.php
+++ shrug.php
@@ -1,11 +1,22 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>shrug</title>
-		<script src="/assets/copyVal.js"></script> 
+		<?php $page_name = "shrug"; ?>
+		<title><?php echo $page_name; ?></title>
+		<link rel="stylesheet" type="text/css" href="/css/main.css">
+		<link rel="stylesheet" type="text/css" href="/css/dropdown.css">
 	</head>
 	<body>
-		<h1> ¯\_(ツ)_/¯ </h1>
-		<button onclick="copyVal('¯\\_(ツ)_/¯')">¯\_(ツ)_/¯</button> 
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/assets/navbar.php'; ?>
+		<div class="container">
+			<div class="blurb">
+			<h1>  ¯\_(ツ)_/¯ </h1>
+			<button onclick="copyVal('¯\\_(ツ)_/¯')">¯\_(ツ)_/¯</button> 
+			<script src="/assets/copyVal.js"></script> 
+			</div>
+		</div>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/assets/webcounter.php';
+		$access_number = visitor($page_name); ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/assets/footbar.php'; ?>
 	</body>
 </html>
