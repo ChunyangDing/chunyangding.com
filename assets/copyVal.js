c4c96ed2046e3d7495bb53414768e5db330b6b28
Chunyang Ding <chunyang.ding@gmail.com>
2020-03-31 21:56:25 -0400

    Maybe I don't need a script tag when the doc is a .js...

--- assets/copyVal.js
+++ assets/copyVal.js
@@ -1,11 +1,9 @@
-<script>
-	function copyVal(value) {
-		var tempInput = document.createElement("input");
-		tempInput.style = "position: absolute; left: -1000px; top: -1000px";
-		tempInput.value = value;
-		document.body.appendChild(tempInput);
-		tempInput.select();
-		document.execCommand("copy");
-		document.body.removeChild(tempInput);
-	} 
-</script>
+function copyVal(value) {
+	var tempInput = document.createElement("input");
+	tempInput.style = "position: absolute; left: -1000px; top: -1000px";
+	tempInput.value = value;
+	document.body.appendChild(tempInput);
+	tempInput.select();
+	document.execCommand("copy");
+	document.body.removeChild(tempInput);
+} 
