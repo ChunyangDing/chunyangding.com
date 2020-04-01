b4d8cc1ca706d3a4eaa7efcd89e2987582f55a57
Chunyang Ding <chunyang.ding@gmail.com>
2020-03-31 22:32:01 -0400

    last fix i swear

--- assets/footbar.php
+++ assets/footbar.php
@@ -1,5 +1,5 @@
 <?php
 echo '<footer class="footer">
-		<p style="display:inline-block;”> made by Chunyang Ding  |  ©2020  | me@chunyangding.com | Page Hits: ', $access_number, ' |</p> 
+		<p> made by Chunyang Ding  |  ©2020  | me@chunyangding.com | Page Hits: ', $access_number, ' |</p> 
 	</footer>';
 ?>
