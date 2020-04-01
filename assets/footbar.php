66f8962d6ccbddeeb137bdfe96be01354ca71f74
Chunyang Ding <chunyang.ding@gmail.com>
2020-03-31 22:18:54 -0400

    this might just break it more???

--- assets/footbar.php
+++ assets/footbar.php
@@ -2,6 +2,7 @@
 echo '<script src="/assets/copyVal.js"></script> 
 
 	<footer class="footer">
-		<p style="display:inline-block;”> made by Chunyang Ding  |  ©2020  | Page Hits: ', $access_number, ' |</p> <button class = "emailCopy" onclick="copyVal(\'me@chunyangding.com\')">me@chunyangding.com</button>  
+		<p style="display:inline-block;”> made by Chunyang Ding  |  ©2020  | Page Hits: ', $access_number, ' |</p> 
+		<button class = "emailCopy" onclick="copyVal(\'me@chunyangding.com\')">me@chunyangding.com</button>  
 	</footer>';
 ?>
