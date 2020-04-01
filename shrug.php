4f1320554c08422547f15ca971964e3ee4cc5094
Chunyang Ding <chunyang.ding@gmail.com>
2020-03-31 21:51:08 -0400

    moved copy function into standalone script, and testing including that script.

--- shrug.php
+++ shrug.php
@@ -2,22 +2,10 @@
 <html>
 	<head>
 		<title>shrug</title>
+		<script src="/assets/copyVal.js"></script> 
 	</head>
 	<body>
 		<h1> ¯\_(ツ)_/¯ </h1>
-		<button onclick="copyShrug('¯\_(ツ)_/¯')">¯\_(ツ)_/¯</button> 
-		
-		<script>
-		function copyShrug(value) {
-			var tempInput = document.createElement("input");
-			tempInput.style = "position: absolute; left: -1000px; top: -1000px";
-			tempInput.value = value;
-			document.body.appendChild(tempInput);
-			tempInput.select();
-			document.execCommand("copy");
-			document.body.removeChild(tempInput);
-		} 
-		</script>
-		
+		<button onclick="copyVal('¯\\_(ツ)_/¯')">¯\_(ツ)_/¯</button> 
 	</body>
 </html>
