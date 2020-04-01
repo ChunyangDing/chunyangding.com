b141d1a40ec9dc0c2c08cd82fbcb1d89002b7b4e
Chunyang Ding <chunyang.ding@gmail.com>
2020-03-31 22:14:11 -0400

    gotta escape me quotes

--- assets/footbar.php
+++ assets/footbar.php
@@ -3,6 +3,6 @@ echo '<script src="/assets/copyVal.js"></script>
 
 	<footer class="footer">
 		<p> made by Chunyang Ding  |  ©2020  | Page Hits: ', $access_number, '</p>
-		<button class = "emailCopy" onclick="copyVal('me@chunyangding.com')">me@chunyangding.com</button>  
-	  </footer>';
+		<button class = "emailCopy" onclick="copyVal(\'me@chunyangding.com\')">me@chunyangding.com</button>  
+	</footer>';
 ?>
