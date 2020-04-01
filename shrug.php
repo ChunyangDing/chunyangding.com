c68af38255b38da398bcc119bc9aba61d0794e26
Chunyang Ding <chunyang.ding@gmail.com>
2020-03-31 21:58:19 -0400

    make that button *pop*

--- shrug.php
+++ shrug.php
@@ -5,7 +5,23 @@
 		<title><?php echo $page_name; ?></title>
 		<link rel="stylesheet" type="text/css" href="/css/main.css">
 		<link rel="stylesheet" type="text/css" href="/css/dropdown.css">
+		
+		<style>
+		.button {
+			background-color: #5e7f79;
+			border: none;
+			color: white;
+			padding: 15px 32px;
+			text-align: center;
+			text-decoration: none;
+			display: inline-block;
+			font-size: 45px;
+			border-radius: 4px;
+		}
+		</style>
+		
 	</head>
+	
 	<body>
 		<?php include $_SERVER['DOCUMENT_ROOT'].'/assets/navbar.php'; ?>
 		<div class="container">
@@ -19,4 +35,5 @@
 		$access_number = visitor($page_name); ?>
 	<?php include $_SERVER['DOCUMENT_ROOT'].'/assets/footbar.php'; ?>
 	</body>
+	
 </html>
