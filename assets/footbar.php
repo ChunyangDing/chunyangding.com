95cd7b6d8eaf6890442937ef7202e138111075cc
Chunyang Ding <chunyang.ding@gmail.com>
2020-03-31 22:12:13 -0400

    buttons can't go inside of p.

--- assets/footbar.php
+++ assets/footbar.php
@@ -2,6 +2,7 @@
 echo '<script src="/assets/copyVal.js"></script> 
 
 	<footer class="footer">
-		<p> made by Chunyang Ding  |  ©2020  | <button class = "emailCopy" onclick="copyVal('me@chunyangding.com')">me@chunyangding.com</button> | Page Hits: ', $access_number, '</p>
+		<p> made by Chunyang Ding  |  ©2020  | Page Hits: ', $access_number, '</p>
+		<button class = "emailCopy" onclick="copyVal('me@chunyangding.com')">me@chunyangding.com</button>  
 	  </footer>';
 ?>
