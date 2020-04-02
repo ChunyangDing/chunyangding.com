12bd01a84571195e448be4bc4b3cf10f46656484
Chunyang Ding <chunyang.ding@gmail.com>
2020-04-01 21:21:03 -0400

    Update weather.php

--- weather.php
+++ weather.php
@@ -11,7 +11,7 @@
 		<div class="container">
 			<div class="blurb">
 			<h1> Here's the weather! </h1>
-			<img src="https://forecast.weather.gov/meteograms/Plotter.php?lat=39.0078&lon=-76.9232&wfo=LWX&zcode=MDZ013&gset=18&gdiff=3&unit=0&tinfo=EY5&ahour=0&pcmd=11011111100000000000000000000000000000000000000000000000000&lg=en&indu=1!1!1!&dd=&bw=&hrspan=48&pqpfhr=6&psnwhr=6", style="width:200px;height:auto;">
+			<img src="https://forecast.weather.gov/meteograms/Plotter.php?lat=39.0078&lon=-76.9232&wfo=LWX&zcode=MDZ013&gset=18&gdiff=3&unit=0&tinfo=EY5&ahour=0&pcmd=11011111100000000000000000000000000000000000000000000000000&lg=en&indu=1!1!1!&dd=&bw=&hrspan=48&pqpfhr=6&psnwhr=6", style="width:2000px;height:auto;">
 			</div>
 		</div>
 	<?php include $_SERVER['DOCUMENT_ROOT'].'/assets/webcounter.php';
