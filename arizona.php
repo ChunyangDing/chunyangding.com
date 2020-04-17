54038ab59232ab723eea5537fc1a6045c30ace52
Chunyang Ding <chunyang.ding@gmail.com>
2020-04-17 11:29:04 -0400

    arizonaaaaAAAAA

---
+++ arizona.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php $page_name = "ARIZONA"; ?>
+		<title><?php echo $page_name; ?></title>
+		<link rel="stylesheet" type="text/css" href="/css/main.css">
+		<link rel="stylesheet" type="text/css" href="/css/dropdown.css">
+	</head>
+	<body>
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/assets/navbar.php'; ?>
+		<div class="container">
+			<div class="blurb">
+				<h1>ARIZONA HYPER EXISTS</h1>
+				<img src="/assets/pics/Arizona/panopticon.png" alt="ARIZONA = PRISON???" style="width:500px;height:auto;" class="center">
+
+				<p> tHIS IS A SECTION OF A NEIGHBORHOOD OF aRIZONA. dOES IT REALLY EXIST, OR IS IT REALLY A "pRISON" FOR INTERDIMENSIONAL CRIMINALS? yOU DECIDE; i BRING U THE tRUU FACTS only.	</p>
+				
+				<img src="/assets/pics/Arizona/coincidence.png" alt="CROP CIRCLES!!!!" style="width:500px;height:auto;" class="center">
+				
+				<p> and.... if you rotate your head..... by 90 DEGREEES.... (the same number of degrees as the HUMAN BODY TEMPERATURE (hmmm....)..... the SAME NEIGHBORHOOD....... looks like..... a plasma ball.......... BIg PhoeNix can't explain this! </p>
+				
+				<img src="/assets/pics/Arizona/ninetyPercent.png" alt="why so few?" style="width:500px;height:auto;" class="center">
+				
+				<p> Also, if there are truly such a diversity of plentiful inhabitable open air ranges, as the government undoubtedly wishes to persuade the general public (ie, the "baaaaasheep"), in what ways would there be such a grotesque concentration of <em>homo sapien</em> cerebrum's in less than 2.048% of the entire land area? Think about that. </p> 
+				
+			</div>
+		</div>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/assets/webcounter.php';
+		$access_number = visitor($page_name); ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/assets/footbar.php'; ?>
+	</body>
+</html>
