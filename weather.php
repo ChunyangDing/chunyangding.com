bb59ac27d00145c68df306bf1dae810929ef26f3
Chunyang Ding <chunyang.ding@gmail.com>
2020-07-27 17:45:22 -0500

    Updated weather to Hyde Park

--- weather.php
+++ weather.php
@@ -11,7 +11,7 @@
 		<div class="container">
 			<div class="blurb">
 			<h1> Here's the weather! </h1>
-			<img src="https://forecast.weather.gov/meteograms/Plotter.php?lat=39.0078&lon=-76.9232&wfo=LWX&zcode=MDZ013&gset=18&gdiff=3&unit=0&tinfo=EY5&ahour=0&pcmd=11011111100000000000000000000000000000000000000000000000000&lg=en&indu=1!1!1!&dd=&bw=&hrspan=48&pqpfhr=6&psnwhr=6", style="width:2000px;height:auto;">
+			<img src="https://forecast.weather.gov/meteograms/Plotter.php?lat=41.7985&lon=-87.5921&wfo=LOT&zcode=ILZ104&gset=10&gdiff=5&unit=0&tinfo=CY6&ahour=0&pcmd=11101111110000000000000000000000000000000000000000000000000&lg=en&indu=1!1!1!&dd=&bw=&hrspan=48&pqpfhr=6&psnwhr=6", style="width:2000px;height:auto;">
 			</div>
 		</div>
 	<?php include $_SERVER['DOCUMENT_ROOT'].'/assets/webcounter.php';
