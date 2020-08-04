bae086899aac4545447d4a17596ad1b06e17398e
Chunyang Ding <chunyang.ding@gmail.com>
2020-08-04 13:57:53 -0500

    i'm bad at php and i should feel bad

--- zelda.php
+++ zelda.php
@@ -142,7 +142,10 @@
 						</a>
 					</div>
 				</div>
+			</div>
+		</div>
 				
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/assets/webcounter.php';
 		$access_number = visitor($page_name); ?>
 	<?php include $_SERVER['DOCUMENT_ROOT'].'/assets/footbar.php'; ?>
 	</body>
