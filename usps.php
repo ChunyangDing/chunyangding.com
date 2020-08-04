93c1d86608d9d05c2735a672e9bb6449d77e87b6
Chunyang Ding <chunyang.ding@gmail.com>
2020-08-04 16:15:45 -0500

    Usps

---
+++ usps.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php $page_name = "USPS"; ?>
+		<title><?php echo $page_name; ?></title>
+		<link rel="stylesheet" type="text/css" href="/css/main.css">
+		<link rel="stylesheet" type="text/css" href="/css/dropdown.css">
+	</head>
+	<body>
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/assets/navbar.php'; ?>
+		<div class="container">
+			<div class="blurb">
+				<h1>Temporary holding page for USPS Info</h1>
+				<p> <a html="https://docs.google.com/document/d/17rGAFYIkbQ1pdHEtDWBf_9TSAsV8pEzORGJCv3XIGlY/edit#">Link to Google Doc if you would like to contribute </a> </p>
+			</div>
+		</div>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/assets/webcounter.php';
+		$access_number = visitor($page_name); ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/assets/footbar.php'; ?>
+	</body>
+</html>
