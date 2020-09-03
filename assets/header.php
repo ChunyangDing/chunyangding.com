7890c99b524c56c94c7f74565bfe5d40e7d3fdd0
Chunyang Ding <chunyang.ding@gmail.com>
2020-09-03 10:45:20 -0500

    favicon tweak

--- assets/header.php
+++ assets/header.php
@@ -7,6 +7,6 @@ echo
 <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
 <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"> </script>
 
-<link rel="icon" type="image/png" href="/favicon.ico"/>
+<link rel="icon" type="image/png" sizes="72x72" href="/favicon.png"/>
 ';
 ?>
