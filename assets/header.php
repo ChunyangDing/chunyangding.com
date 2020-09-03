3f6d0fc6a61803e129a0ecf538323efd8915d7ad
Chunyang Ding <chunyang.ding@gmail.com>
2020-09-03 10:26:14 -0500

    Starting process of converting header tests

---
+++ assets/header.php
@@ -0,0 +1,12 @@
+<?php
+echo 
+'
+<link rel="stylesheet" type="text/css" href="/css/main.css">
+<link rel="stylesheet" type="text/css" href="/css/dropdown.css">
+
+<script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
+<script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"> </script>
+
+<link rel="icon" type="image/png" href="/favicon.ico"/>
+';
+?>
